ea0d2e39e52d7a3a41106adb434f1f6b524e4274
Diego <checaoduwell@gmail.com>
2021-08-18 21:30:22 -0300

    fix

--- app/src/main/java/br/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/br/com/jetpack/compose/MainActivity.kt
@@ -3,36 +3,27 @@ package br.com.jetpack.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import br.com.jetpack.compose.ui.theme.MyApplicationTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Text(text = "Primeiro teste jetpack compose")
+                Spacer(modifier = Modifier.padding(10.dp))
+                Button(onClick = {}) {
+                    Text(text = "Click aqui")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        Greeting("Android")
     }
 }
