686170aafe13a541f9df6f388eea5a5a664dd100
Diego <checaoduwell@gmail.com>
2021-08-19 20:55:07 -0300

    fix

--- app/src/main/java/br/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/br/com/jetpack/compose/MainActivity.kt
@@ -1,23 +1,23 @@
 package br.com.jetpack.compose
 
-import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale.Companion.Crop
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,32 +32,42 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-    @Preview(showBackground = true, backgroundColor = 0xFF00FF00)
+    @Preview
     @Composable
     fun Container() {
         Column(
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .background(Color.White)
         ) {
             Column(
                 modifier = Modifier
                     .padding(16.dp)
-                    .background(Color.Gray)
             ) {
-                Text(
-                    color = Color.Blue,
-                    fontStyle = FontStyle.Normal,
-                    style = TextStyle.Default,
-                    text = "Primeiro teste jetpack compose"
-                )
                 Image(
-                    painterResource(R.mipmap.sym_def_app_icon),
+                    painterResource(R.mipmap.ic_launcher),
                     contentDescription = "",
                     contentScale = Crop
                 )
-                Spacer(modifier = Modifier.padding(10.dp))
-                Button(onClick = {}) {
-                    Text(text = "Click aqui")
-                }
+//                Spacer(modifier = Modifier.padding(10.dp))
+                Text(
+                    text = getString(R.string.string_test_name),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.Start),
+                    color = Color.Blue,
+                    fontStyle = FontStyle.Normal,
+                    style = MaterialTheme.typography.body2
+                )
+                Text(
+                    text = getString(R.string.string_test_job),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentWidth(Alignment.Start),
+                    color = Color.Blue,
+                    fontStyle = FontStyle.Normal,
+                    style = MaterialTheme.typography.body1
+                )
             }
 
         }
