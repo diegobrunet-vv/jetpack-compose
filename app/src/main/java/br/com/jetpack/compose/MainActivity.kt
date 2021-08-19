f2cec53647670cb79ef08579af4e244e17accc58
Diego <checaoduwell@gmail.com>
2021-08-18 22:03:48 -0300

    fix

--- app/src/main/java/br/com/jetpack/compose/MainActivity.kt
+++ app/src/main/java/br/com/jetpack/compose/MainActivity.kt
@@ -3,12 +3,17 @@ package br.com.jetpack.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +23,29 @@ class MainActivity : ComponentActivity() {
             Column(
                 modifier = Modifier.padding(16.dp)
             ) {
-                Text(text = "Primeiro teste jetpack compose")
-                Spacer(modifier = Modifier.padding(10.dp))
-                Button(onClick = {}) {
-                    Text(text = "Click aqui")
+                Column(
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .background(Color.Gray)
+                ) {
+                    Text(
+                        color = Color.Blue,
+                        fontStyle = FontStyle.Normal,
+                        style = TextStyle.Default,
+                        text = "Primeiro teste jetpack compose"
+                    )
+                    Image(
+                        bitmap = imageFromResource(
+                            res = resources,
+                            resId = R.drawable.ic_launcher_foreground
+                        )
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(onClick = {}) {
+                        Text(text = "Click aqui")
+                    }
                 }
+
             }
         }
     }
